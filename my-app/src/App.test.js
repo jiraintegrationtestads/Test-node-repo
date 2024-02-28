4eaa4cc1c56aa8ec193ed262413fd2b66514ab58
vincent-wang-adserve <vincent.wang@adserve.com>
2024-02-28 22:11:38 +0000

    without registry url to test if breaks

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { ApplicationContextProvider } from '@adserve/application-context';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<ApplicationContextProvider><App /></ApplicationContextProvider>);
   const linkElement = screen.getByText(/learn react/i);
-  
+
   expect(linkElement).toBeInTheDocument();
 });
